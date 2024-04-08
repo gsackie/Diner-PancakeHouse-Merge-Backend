6de9f49f32928026069b3022ddcd2f6fe0884d14
gsack <gsackie@iu.edu>
2024-04-07 23:52:38 -0400

    Hw

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/Application.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Diner Pancake House Merge application!");
 		SpringApplication.run(Application.class, args);
 	}
 }
